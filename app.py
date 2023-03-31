dd8195a6cb4677c9cde7712f195464d1919d66e8
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-31 11:23:00 +0300

    hotfix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from calculator_bot_aiogram.calculator.loader import bot
+from calculator.loader import bot
 
 if __name__ == "__main__":
-    from calculator_bot_aiogram.calculator.loader import dp
+    from calculator.loader import dp
 
     dp.run_polling(bot)
